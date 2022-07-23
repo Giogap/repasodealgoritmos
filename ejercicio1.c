861bc18c529a5779e84f8cce9bfeef1e416ce6c0
Giovanni <giogap@gmail.com>
2022-07-23 04:55:53 -0500

    ok

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /** 
  * Realizar un algoritmo que lea tres numeros, si el primero es positivo calcule el producto de los 
 tres numeros y en otro caso la suma. 
@@ -5,19 +8,19 @@ Se usan tres variables enteras - Numero1, Numero2, Numero3, en las que se leen l
 variables Producto y Suma en las que se calcula, o bien el producto, o bienla suma. El algoritmo que 
 resuelve el problema es el siguiente.*/
 
-#include <stdio.h>
-
 int produc_sum(int a, int b, int c)
 {
-    int producto;
-    int suma;
+    int producto = 0, suma = 0; 
 
-    if (a >= 0)
+    if (a > 0)
     {
         producto = a * b * c;
+        return(producto);
     }
     else
     {
         suma = a + b + c;
+        return(suma);
     }
+    return(0);
 }
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n;
+
+    n = produc_sum(-5, 7, 3);
+    printf("%d\n", n);
+    return(0);
+}
